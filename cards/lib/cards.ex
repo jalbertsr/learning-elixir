bbae6a5f53f7af30c3cd9dd8af3f2c260c97f2f6
Joan Albert Segura <jalbertsr@protonmail.ch>
2018-01-28 19:59:15 +0100

    docs for elixir

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of playing cards 
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -19,6 +26,18 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  @doc """
+    Divides a deck into a hand and the remainder of the the deck.
+    The `hand_size` argument indicates how many cards should be in the hand
+  
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
 
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
